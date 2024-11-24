aa3f6753222e567d9660d166d20441bb33914bc4
Skandar <skandarslugin@gmail.com>
2024-11-24 10:06:24 +0200

    refactor: replace <style> with @vanilla-extract/css styles in Callout component
    When we use Astro component inside MDX, @vanilla-extract/css doesn't create styles for component (but generate class). Importing components in <Content> fix this issue.

---
+++ src/components/ArticleComponents.ts
@@ -0,0 +1,11 @@
+import Callout from "~/components/Callout/Callout.astro";
+import { Image, Picture } from "astro:assets";
+
+/**
+ * This object is used to import all possible components in Article.
+ */
+export const components = {
+  Callout,
+  Image,
+  Picture,
+};
