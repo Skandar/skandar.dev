28d83b5a24004f74814a335e3f08616d88a94f83
Skandar <skandarslugin@gmail.com>
2024-12-02 19:05:50 +0200

    content: Article "Object mutation debugging"

--- src/components/ArticleComponents.ts
+++ src/components/ArticleComponents.ts
@@ -1,5 +1,6 @@
 import Callout from "~/components/Callout/Callout.astro";
 import { Image, Picture } from "astro:assets";
+import Link from "./Link/Link.astro";
 
 /**
  * This object is used to import all possible components in Article.
@@ -8,4 +9,5 @@ export const customComponents = {
   Callout,
   Image,
   Picture,
+  Link,
 };
