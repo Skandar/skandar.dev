c5a54ad40815de68107a7b9cc052d0d71a4031cd
Skandar <skandarslugin@gmail.com>
2026-06-20 13:13:50 +0300

    refactor: remove getSingularCollectionForm utility

--- src/utils/getSingularCollectionForm.ts
+++
@@ -1,7 +0,0 @@
-const collections = {
-  articles: "articles",
-} as const;
-
-export const getSingularCollectionForm = (
-  collectionName: keyof typeof collections,
-) => collections[collectionName] || collectionName;
